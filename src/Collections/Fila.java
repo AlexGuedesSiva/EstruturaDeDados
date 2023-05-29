3748cff6209fe439284289383a101db79df82244
AlexGuedesSiva <alex__guedes@hotmail.com>
2023-05-29 06:57:32 -0300

    add implementação das class Fila e Pilha

--- src/Collections/Fila.java
+++ src/Collections/Fila.java
@@ -1,6 +1,57 @@
 package Collections;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+Collection Queue:
+    A interface Collection Queue representa uma estrutura de dados do tipo fila (queue) em Java. Uma fila é uma coleção
+    ordenada de elementos que segue o princípio FIFO (First-In-First-Out), o que significa que o primeiro elemento
+    adicionado à fila será o primeiro a ser removido. A interface Collection Queue estende a interface Collection e
+    adiciona métodos específicos para manipular filas.
+
+Principais métodos da interface Collection Queue:
+
+    add(elemento): Adiciona um elemento à fila. Se não for possível adicionar o elemento, uma exceção é lançada.
+    offer(elemento): Adiciona um elemento à fila. Retorna true se o elemento foi adicionado com sucesso, ou false caso
+    contrário.
+    remove(): Remove o elemento da frente da fila e o retorna. Se a fila estiver vazia, uma exceção é lançada.
+    poll(): Remove o elemento da frente da fila e o retorna. Retorna null se a fila estiver vazia.
+    element(): Retorna o elemento da frente da fila sem removê-lo. Se a fila estiver vazia, uma exceção é lançada.
+    peek(): Retorna o elemento da frente da fila sem removê-lo. Retorna null se a fila estiver vazia.
+
+ */
+
+
 public class Fila {
+    public static void main(String[] args) {
+
+        Queue<String> fila = new LinkedList<>();
+
+        fila.add("Sara"); // Lança uma Exceção.
+        fila.offer("Levi"); // Retorna Null
+        fila.offer("Isaque");
+        fila.offer("Daniel");
+        fila.offer("Lea");
+        fila.offer("Raquel");
+        fila.offer("José");
 
 
+        System.out.println(fila.peek()); // Retorna null
+        System.out.println(fila.element()); // Lança uma Exceção.
+
+        System.out.println(fila.size());// Retorna Tamanho da fila
+//        fila.clear(); // Limpar a fila.
+//        System.out.println(fila.isEmpty()); // Retorna Sempre True, pois fila esta vazia.
+
+        System.out.println(fila.poll());// Retorna o first elemento e remove.
+        System.out.println(fila.poll());
+        System.out.println(fila.poll());
+        System.out.println(fila.poll());
+        System.out.println(fila.poll());
+        System.out.println(fila.remove()); // Lança uma Exceção.
+        System.out.println(fila.poll()); // Retorna Null, se a fila estiver vazia
+
+
+    }
 }
--- src/Collections/Pilha.java
+++ src/Collections/Pilha.java
@@ -1,36 +1,74 @@
 package Collections;
 
+import java.sql.SQLOutput;
 import java.util.ArrayDeque;
 import java.util.Deque;
+
+/*
+A interface Deque (Double Ended Queue)
+    representa uma fila de elementos onde é possível adicionar e remover elementos tanto no início quanto no final da
+    fila. Isso permite que ela seja usada como uma pilha (estrutura LIFO - Last-In-First-Out) ou uma fila
+    (estrutura FIFO - First-In-First-Out), dependendo da forma como os elementos são manipulados.
+
+ArrayDeque
+    é uma implementação concreta da interface Deque que utiliza um array redimensionável para armazenar seus elementos.
+    Ela fornece acesso rápido tanto ao início quanto ao final da fila, além de oferecer um desempenho eficiente na
+    adição e remoção de elementos nas extremidades.
+
+A classe ArrayDeque
+    é uma estrutura de dados de tamanho dinâmico, o que significa que ela pode aumentar ou diminuir automaticamente
+    sua capacidade à medida que elementos são adicionados ou removidos. Isso é feito internamente pela realocação do
+    array subjacente.
+
+    Além dos métodos herdados da interface Deque, como addFirst(), addLast(), removeFirst(), removeLast(), peekFirst(),
+    peekLast(), entre outros, ArrayDeque também implementa a interface Queue, permitindo que ela seja usada como uma
+    fila convencional.
+
+A principal vantagem de usar ArrayDeque
+    em relação a outras implementações de fila, como LinkedList, é seu desempenho superior para operações de adição e
+    remoção em ambas as extremidades da fila.
+
+
+ */
+
 
 public class Pilha {
     public static void main(String[] args) {
 
+        // EXEMPLO 1
+        System.out.println("EXEMPLO 1");
+        Deque<String> deque = new ArrayDeque<>();
+
+        deque.addFirst("Elemento 1");
+        deque.addLast("Elemento 2");
+        deque.addLast("Elemento 3");
+
+        System.out.println(deque.peekFirst()); // Saída: Elemento 1
+        System.out.println(deque.peekLast());  // Saída: Elemento 3
+
+        deque.removeFirst();
+        deque.removeLast();
+
+        System.out.println(deque.peekFirst()); // Saída: Elemento 2
+
+        System.out.println();
+        // EXEMPLO 2
+        System.out.println("EXEMPLO 2");
         Deque<String> livros = new ArrayDeque<String>();
 
         livros.add("O Pequeno Principe");
-        livros.push("Don Quixote"); //adiciona um elemento no topo da pilha.
+        livros.push("Don Quixote");
         livros.push("O Hobbit");
 
+        System.out.println(livros.peek());// Retorna o elemento da frente da fila sem removê-lo
+        System.out.println(livros.element());// Retorna o elemento da frente da fila sem removê-lo.
 
-        System.out.println("Elemento removido --> " + livros.pop()); // remove e retorna o elemento no topo da pilha
-        System.out.println("Elemento no topo --> " + livros.peek());
-        System.out.println("Tamanho da pilha " + livros.size());
-
-
-        System.out.println(livros.element()); // Retorna uma exceção caso vazio
-        System.out.println(livros.pollFirst());
-        System.out.println(livros.pollFirst());
-        System.out.println(livros.peek()); // Retorna null caso vazio
-
-//		System.out.println(livros.pop());
-//		System.out.println(livros.remove());
-
-//		livros.size(); retorna o numero de elemetos na pilha
-//		livros.clear(); // remove todos elemetos da pilha
-//		livros.contains();
-//		livros.isEmpty(); verifica se a pilha esta vazia
-
+        System.out.println(livros.pop());// remove e retorna o primeiro elemento deste deque.Este método é equivalente a removeFirst().
+        System.out.println(livros.poll()); // Remove o elemento da frente da fila e o retorna.
+        System.out.println(livros.poll());
+        System.out.println(livros.poll());
+        System.out.println(livros.poll());
+    //  System.out.println(livros.remove()); // Lança uma Exceção.
 
     }
 }
